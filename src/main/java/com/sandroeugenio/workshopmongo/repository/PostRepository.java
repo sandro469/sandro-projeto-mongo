8c868c6919d3dbf54e5e63db73fe86f5756122cd
san <sandrolocal41@gmail.com>
2026-07-15 23:03:02 -0300

    Criando entity Post com user alinhado

--- src/main/java/com/sandroeugenio/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/sandroeugenio/workshopmongo/repository/PostRepository.java
@@ -3,9 +3,9 @@ package com.sandroeugenio.workshopmongo.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.sandroeugenio.workshopmongo.domain.User;
+import com.sandroeugenio.workshopmongo.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String>{
 
 }
