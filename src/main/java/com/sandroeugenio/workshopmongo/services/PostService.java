f5f5a2366d6587497cbb39a8a205d34cc0c147ed
san <sandrolocal41@gmail.com>
2026-07-17 00:28:01 -0300

    Obtendo post por id

---
+++ src/main/java/com/sandroeugenio/workshopmongo/services/PostService.java
@@ -0,0 +1,27 @@
+package com.sandroeugenio.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sandroeugenio.workshopmongo.domain.Post;
+import com.sandroeugenio.workshopmongo.repository.PostRepository;
+import com.sandroeugenio.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findAll() {
+        return repo.findAll();
+    }
+}
