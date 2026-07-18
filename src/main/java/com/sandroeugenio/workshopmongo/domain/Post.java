0da542fc90b93d54bba2648de13ff1dfac8c2c4a
sandro <sandrolocal41@gmail.com>
2026-07-17 21:16:31 -0300

    Acrescentando comentarios aos Posts

--- src/main/java/com/sandroeugenio/workshopmongo/domain/Post.java
+++ src/main/java/com/sandroeugenio/workshopmongo/domain/Post.java
@@ -1,25 +1,31 @@
 package com.sandroeugenio.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.sandroeugenio.workshopmongo.dto.AuthorDTO;
+import com.sandroeugenio.workshopmongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
-
 	private static final long serialVersionUID = 1L;
+	
 
 	@Id
 	private String id;
 	private Date date;
-	private String title;
+	private String title; 
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
+	
 
 	public Post() {
 	}
@@ -72,6 +78,13 @@ public class Post implements Serializable {
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
 	}
+	public List<CommentDTO> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<CommentDTO> comments) {
+		this.comments = comments;
+	}
 
 	@Override
 	public int hashCode() {
@@ -93,4 +106,6 @@ public class Post implements Serializable {
 
 		return Objects.equals(id, other.id);
 	}
+
+	
 }
