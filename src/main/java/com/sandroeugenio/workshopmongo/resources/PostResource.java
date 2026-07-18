3843e62ed2d1f29ad13748e730fc1b2b7acf56d9
sandro <sandrolocal41@gmail.com>
2026-07-17 22:02:52 -0300

    Consulta simples com Query methods

--- src/main/java/com/sandroeugenio/workshopmongo/resources/PostResource.java
+++ src/main/java/com/sandroeugenio/workshopmongo/resources/PostResource.java
@@ -5,8 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sandroeugenio.workshopmongo.domain.Post;
@@ -28,6 +29,14 @@ public class PostResource {
     @GetMapping
     public ResponseEntity<List<Post>> findAll() {
         List<Post> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(
+            @RequestParam(value = "text", defaultValue = "") String text) {
+
+        List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
 }
--- src/main/java/com/sandroeugenio/workshopmongo/services/PostService.java
+++ src/main/java/com/sandroeugenio/workshopmongo/services/PostService.java
@@ -24,4 +24,8 @@ public class PostService {
     public List<Post> findAll() {
         return repo.findAll();
     }
+    
+    public List<Post> findByTitle(String text) {
+    	return repo.findByTitleContainingIgnoreCase(text);
+    }
 }
